6d3506f0cd7d7ba7151acef1f2cd5954517b616a
Ihtasham <chowdhuryihtasham42@gmail.com>
2022-05-06 18:47:14 +0100

    files setup

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const mongoose = require("express");
-const dotenv = require("dotenv")
-
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
+  console.log("MongoDB connected");
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
+
+app.use(express.json());
