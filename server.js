5ad37de424c690bfe08838206c1959f55773cb72
Ihtasham <chowdhuryihtasham42@gmail.com>
2022-05-20 19:12:19 +0100

    path changes

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
